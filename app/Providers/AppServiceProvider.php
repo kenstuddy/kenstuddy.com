989522cd94729e1e90b653b49ac23945b52ec5f1
Ken Studdy <kenstuddy@users.noreply.github.com>
2018-05-18 21:39:24 -0400

    Decrease default string length
    Allows MySQL versions older than 5.7.7 on Windows to create indexes for migrations.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //Set default string length to 191 to allow MySQL versions older than 5.7.7 on Windows to create indexes for migrations.
+        Schema::defaultStringLength(191);
     }
 
     /**
