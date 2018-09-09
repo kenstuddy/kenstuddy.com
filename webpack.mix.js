7a947ae2ae88be692c8c849ca258cde7d86609d1
Ken Studdy <kenstuddy@users.noreply.github.com>
2018-09-08 23:24:09 -0400

    Remove jQuery dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,27 +14,10 @@ let mix = require('laravel-mix');
 mix.styles([
     'node_modules/bootstrap/dist/css/bootstrap.css',
     'node_modules/normalize.css/normalize.css',
-    'node_modules/magnific-popup/dist/magnific-popup.css',
     'node_modules/font-awesome/css/font-awesome.css',
-    'node_modules/vegas/dist/vegas.css',
     'resources/assets/css/fonts.css',
     'resources/assets/css/main.css'
 ], 'public/css/app.css')
-    .js([
-    'resources/assets/js/app.js',
-    'resources/assets/js/contact_me.js',
-    'resources/assets/js/main.js',
-    'resources/assets/js/jquery.nav.js',
-    'resources/assets/js/jqBootstrapValidation.js',
-    'resources/assets/js/waypoints.js',
-    'resources/assets/js/jquery.sticky.js',
-    'node_modules/bootstrap/dist/js/bootstrap.js',
-    'node_modules/vegas/dist/vegas.js',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-    'node_modules/isotope-layout/dist/isotope.pkgd.js',
-    'node_modules/jquery.scrollto/jquery.scrollTo.js'
-], 'public/js/app.js')
-   .extract([
-    'jquery',
-    'vue'
-]);
+   .js([
+    'resources/assets/js/app.js'
+], 'public/js/app.js').extract(['vue']);
