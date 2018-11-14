9c5d2bd35271bfc85a7052facb22d237e778cb0d
kenstuddy <kenstuddy@users.noreply.github.com>
2018-11-13 19:24:02 -0500

    Removed old navbar

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -19,28 +19,3 @@
         </div>
       </div>
 </nav>
-<!--
-<div class="navbar navbar-expand-md navbar-light fixed-top" role="navigation">
-        <span class="icon-bar"></span>
-        <span class="sr-only">Toggle navigation</span>
-        <button type="button" class="navbar-toggler collapsed" data-toggle="collapse"
-        data-target=".navbar-collapse">&#x2630;</button>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav flex-row ml-md-auto d-md-flex" id="main-menu">
-                <li class="nav-item"><a href="#page-welcome" class="nav-link">Home</a>
-                </li>
-                <li class="nav-item"><a href="#page-profile" class="nav-link">Profile</a>
-                </li>
-                <li class="nav-item"><a href="#page-education" class="nav-link">Education</a>
-                </li>
-                <li class="nav-item"><a href="#page-experience" class="nav-link">Experience</a>
-                </li>
-                <li class="nav-item"><a href="#page-portfolio" class="nav-link">Portfolio</a>
-                </li>
-                <li class="nav-item"><a href="#page-contact" class="nav-link">Contact</a>
-                </li>
-            </ul>
-        </div>
-    
-</div>
--->
