3e6a70558cc456cdcd48a465486f4313e1e2d503
Ken Studdy <kenstuddy@gmail.com>
2022-12-28 20:36:56 -0500

    Removed GitDown references

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -4,9 +4,6 @@
         @include('layouts.header')
         @extends('layouts.headermeta')
         @section('title', $page['page_title'] . " - " . $article->title)
-        @section('css')
-            @gitdown
-        @endsection
     </head>
     <body class="{{ theme("bg-darker", "bg-white") }}">
         @include('layouts.nav')
